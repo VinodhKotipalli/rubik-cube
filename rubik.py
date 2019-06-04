811bf59e0620188f8320686a5571575c9b316ee8
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2019-06-04 14:26:24 -0700

    added logic to store the explored states in a tree, optimised for minimum tree height. and depth of the node containing the state is used calculate distance from solved cube

--- rubik.py
+++ rubik.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from os import path 
 import abc
 import random
-import sys
+
+from anytree import *
 
 import numpy as np
 
@@ -19,11 +20,6 @@ class Face:
 	D = 4
 	B = 5
 
-	
-class InnerLayer:
-		O = 0
-		P = 1
-
 
 class Action:
 	ROT90 = 0
@@ -33,9 +29,17 @@ class Action:
 
 
 class Edge(abc.ABC):
+	
+	@property 
+	def index(self):
+		return self._index
+
+	@index.setter
+	def index(self, value):
+		self._index = value	
 
 	@abc.abstractmethod
-	def rot90(self):
+	def rot90(self, state):
 		...
 	
 	@abc.abstractmethod
@@ -54,17 +58,7 @@ class CenterLR(Edge):
 	def __init__(self, index=1):
 		self._index = index
 		
-	@property 
-	def index(self):
-		return self._index
-
-	@index.setter
-	def index(self, value):
-		self._index = value
-
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.F, self.index, :] = state[Face.U, self.index, :]
 		nextState[Face.D, self.index, :] = state[Face.F, self.index, :]
@@ -73,8 +67,6 @@ class CenterLR(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.F, self.index, :] = state[Face.D, self.index, :]
 		nextState[Face.D, self.index, :] = state[Face.B, self.index, :]
@@ -88,17 +80,7 @@ class CenterFB(Edge):
 	def __init__(self, index=1):
 		self._index = index
 		
-	@property 
-	def index(self):
-		return self._index
-
-	@index.setter
-	def index(self, value):
-		self._index = value
-
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, self.index, :] = state[Face.D, self.index, :]
 		nextState[Face.D, self.index, :] = state[Face.R, self.index, :]
@@ -107,8 +89,6 @@ class CenterFB(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, self.index, :] = state[Face.U, self.index, :]
 		nextState[Face.D, self.index, :] = state[Face.L, self.index, :]
@@ -121,18 +101,8 @@ class CenterUD(Edge):
 
 	def __init__(self, index=1):
 		self._index = index
-		
-	@property 
-	def index(self):
-		return self._index
-
-	@index.setter
-	def index(self, value):
-		self._index = value
 
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, self.index, :] = state[Face.B, self.index, :]
 		nextState[Face.F, self.index, :] = state[Face.L, self.index, :]
@@ -141,8 +111,6 @@ class CenterUD(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, self.index, :] = state[Face.F, self.index, :]
 		nextState[Face.F, self.index, :] = state[Face.R, self.index, :]
@@ -155,7 +123,6 @@ class FrontFace(Edge):
 
 	def rot90(self, state):
 		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.F, :, :] = np.rot90(nextState[Face.F, :, :], 1)
 		nextState[Face.L, m - 1, :] = state[Face.D, m - 1, :]
@@ -166,7 +133,6 @@ class FrontFace(Edge):
 
 	def rot270(self, state):
 		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.F, :, :] = np.rot90(nextState[Face.F, :, :], -1)
 		nextState[Face.L, m - 1, :] = state[Face.U, m - 1, :]
@@ -196,8 +162,6 @@ class FrontFace(Edge):
 class BackFace(Edge):
 
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.B, :, :] = np.rot90(nextState[Face.B, :, :], 1)
 		nextState[Face.L, 0, :] = state[Face.U, 0, :]
@@ -207,8 +171,6 @@ class BackFace(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.B, :, :] = np.rot90(nextState[Face.B, :, :], -1)
 		nextState[Face.L, 0, :] = state[Face.D, 0, :]
@@ -238,7 +200,6 @@ class RightFace(Edge):
 
 	def rot90(self, state):
 		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.R, :, :] = np.rot90(nextState[Face.R, :, :], 1)
 		nextState[Face.F, m - 1, :] = state[Face.D, m - 1, :]
@@ -249,7 +210,6 @@ class RightFace(Edge):
 
 	def rot270(self, state):
 		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.R, :, :] = np.rot90(nextState[Face.R, :, :], -1)
 		nextState[Face.F, m - 1, :] = state[Face.U, m - 1, :]
@@ -278,8 +238,6 @@ class RightFace(Edge):
 class LeftFace(Edge):
 
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, :, :] = np.rot90(nextState[Face.L, :, :], 1)
 		nextState[Face.F, 0, :] = state[Face.U, 0, :]
@@ -289,8 +247,6 @@ class LeftFace(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.L, :, :] = np.rot90(nextState[Face.L, :, :], -1)
 		nextState[Face.F, 0, :] = state[Face.D, 0, :]
@@ -314,12 +270,11 @@ class LeftFace(Edge):
 		nextState[Face.B, :, :] = state[Face.U, :, :]
 		nextState[Face.U, :, :] = state[Face.F, :, :]
 		return nextState
-
+			
 
 class DownFace(Edge):
 
 	def rot90(self, state):
-		m = np.size(state, axis=1)
 		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.D, :, :] = np.rot90(nextState[Face.D, :, :], 1)
@@ -331,7 +286,6 @@ class DownFace(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
 		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.D, :, :] = np.rot90(nextState[Face.D, :, :], -1)
@@ -361,8 +315,6 @@ class DownFace(Edge):
 class UpFace(Edge):
 
 	def rot90(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.U, :, :] = np.rot90(nextState[Face.U, :, :], 1)
 		nextState[Face.L, :, 0] = state[Face.F, :, 0]
@@ -372,8 +324,6 @@ class UpFace(Edge):
 		return nextState
 
 	def rot270(self, state):
-		m = np.size(state, axis=1)
-		n = np.size(state, axis=2)
 		nextState = np.copy(state)
 		nextState[Face.U, :, :] = np.rot90(nextState[Face.U, :, :], -1)
 		nextState[Face.L, :, 0] = state[Face.B, :, 0]
@@ -398,7 +348,7 @@ class UpFace(Edge):
 		nextState[Face.B, :, :] = state[Face.R, :, :]
 		return nextState
 
-
+		
 class Rubik(object):
 
 	def __init__(self, style=Style.BASIC, size=[2, 2]):
@@ -438,9 +388,13 @@ class Rubik(object):
 	def state(self, value):
 		self._state = value
 
+	def resetState(self):
+		self.state = np.copy(self.baseState)
+		
 	def initState(self):
 		m = self.size[0]
 		n = self.size[1]
+		e = 6 + (m - 2) * 2 + (n - 2)
 
 		if self.style == Style.BASIC:
 			self.state = np.zeros(shape=(6, m, n), dtype=int)
@@ -451,10 +405,6 @@ class Rubik(object):
 			up = UpFace()
 			down = DownFace()
 			
-			centerLR = CenterLR(m % 2)
-			centerFB = CenterFB(m % 2)
-			centerUD = CenterUD(n % 2)
-
 			self.edges = {
 				Face.F: front,
 				Face.B: back,
@@ -464,16 +414,29 @@ class Rubik(object):
 				Face.D: down
 			}
 			
+			j = len(list(self.edges.keys()))
+			for i in range(1, m - 1):
+				self.edges.update({j:CenterLR(i)})
+				j = j + 1
+				self.edges.update({j:CenterFB(i)})
+				j = j + 1
+				
+			for i in range(1, n - 1):
+				self.edges.update({j:CenterUD(i)})
+				j = j + 1
+			
 			for i in range(6):
 				self.state[i, :, :] = i
-			self.spinRubik(np.size(self.state))
+			
+			# print('e = %d self.edges = %d' % (e, len(self.edges)))
+			
+			self.baseState = np.copy(self.state)
 	
 	def isSolved(self):
 		m = self.size[0]
 		n = self.size[1]
 
 		if self.style == Style.BASIC:
-
 			for i in range(6):
 				iFace = self.state[i, :, :]
 				if np.sum(iFace) != (self.state[i, 0, 0] * m * n):
@@ -485,7 +448,36 @@ class Rubik(object):
 	def isNotSolved(self):
 		result = not self.isSolved()
 		return result
-
+	
+	def isIndependentFeature(self, i, j, k):
+		m = self.size[0]
+		n = self.size[1]
+		if (self.style == Style.BASIC):
+			if m % 2 == 1 and n % 2 == 1 and self.isCenterSquare(j, k):
+				return False
+			elif (i == Face.L or i == Face.R) and self.isCornerSquare(j, k):
+				return False
+			else: 
+				return True
+		else:
+			return True
+		
+	def isCenterSquare(self, j, k):
+		m = self.size[0]
+		n = self.size[1]
+		if (self.style == Style.BASIC and j == int(m / 2) and k == int(n / 2)):
+			return True
+		else:
+			return False
+		
+	def isCornerSquare(self, j, k):
+		m = self.size[0]
+		n = self.size[1]
+		if self.style == Style.BASIC and (j == 0 or j == m - 1) and (k == 0 or k == n - 1):
+			return True
+		else:
+			return False		
+	
 	def printState(self):
 		if self.style == Style.BASIC:
 			print("Front Face: ")
@@ -513,7 +505,7 @@ class Rubik(object):
 		elif action == Action.SPIN270:
 			self.state = edgeObject.spin270(self.state)
 	
-	def doRandomActions(self, count, low=0, high=11):
+	def doRandomActions(self, count=1, low=0, high=11):
 		m = self.size[0]
 		n = self.size[1]		
 		e = 6 + (m - 2) * 2 + (n - 2)		
@@ -522,7 +514,7 @@ class Rubik(object):
 			edge = r % ((high + 1) / e)
 			action = int(r / e)
 			self.doAction(edge, action)
-		self.reorient()
+		self.latestAction = r
 		return self
 
 	def spinRubik(self, count):
@@ -541,15 +533,35 @@ class Rubik(object):
 			for i in range(6):
 				iSum = np.sum(self.state[i, :, :])
 				iMid = self.state[i, m % 2 + 1, n % 2 + 1]
-				if iSum < fSum and m % 2 == 0 and n % 2 :
-					fSum = iSum
-					f = i
-				if iMid == Face.F and m % 2 == 1 and n % 2 == 1:
-					f = i
+				if m % 2 == 0 and n % 2 == 0:
+					if iSum < fSum:
+						fSum = iSum
+						f = i
+				elif m % 2 == 1 and n % 2 == 1:
+					if iMid == Face.F:
+						f = i
 		return f
-	
-	def reorient(self):
+
+	def getFrontFaceAsString(self):
 		f = self.getFrontFace()
+		if(f == Face.F):
+			return 'F'
+		if(f == Face.B):
+			return 'B'
+		if(f == Face.L):
+			return 'L'
+		if(f == Face.R):
+			return 'R'
+		if(f == Face.U):
+			return 'U'
+		if(f == Face.D):
+			return 'D'		
+	
+	def reorient(self, withFrontFace=None):
+		if withFrontFace != None:
+			f = withFrontFace
+		else:
+			f = self.getFrontFace()
 		if(f == Face.F):
 			pass
 		elif(f == Face.B):
@@ -563,65 +575,171 @@ class Rubik(object):
 			self.doAction(Face.U, Action.SPIN270)
 		elif(f == Face.R):
 			self.doAction(Face.U, Action.SPIN90)
-	
+
 	def stateInCsv(self):
 		row_csv = ""
 		m = self.size[0]
 		n = self.size[1]
 		
 		if self.style == Style.BASIC:
-			for i in range(6):
+			for i in [Face.F, Face.B, Face.L, Face.R, Face.U, Face.D]:
 				for j in range(m):
 					for k in range(n):
 						row_csv = row_csv + str(self.state[i, j, k]) + ","
-			
 			return row_csv[:-1]
-			
 
-def randNormInt(low, high, count):
-
-	mid = (low + high) / 2
-	randomNums = np.random.normal(loc=mid, scale=(high - mid) / 4 , size=count)
-	return np.round(randomNums)
+	def setState(self, inputVector):
+		m = self.size[0]
+		n = self.size[1]
+		
+		newState = np.zeros(shape=(6, m, n), dtype=int)
+		index = 0
+		if self.style == Style.BASIC:
+			for i in [Face.F, Face.B, Face.L, Face.R, Face.U, Face.D]:
+				for j in range(m):
+					for k in range(n):
+						newState[i, j, k] = inputVector[index]
+						index = index + 1
+										
+			self.state = np.copy(newState)
 
 
 if __name__ == '__main__':
 	
-	m = 2
-	n = 2
-	e = 6 + (m - 2) * 2 + (n - 2)
-	maxK = 100000
-	actionsCount = randNormInt(1, 100, maxK)
-	data = dict() 
-	
 	filedir = path.dirname(path.abspath(__file__)).replace('\\', '/').replace('C:', '')
-	outpath = filedir + '/basic' + str(m) + 'x' + str(n) + 'RubikCube.csv'
+	maxKValues = {2:99988, 3:224988, 4:399988, 5:624988}
+	# #maxKValues = {2:100, 3:100, 4:100, 5:100}
+	minActions = {2:1, 3:1, 4:1, 5:1}
+	maxActions = {2:100, 3:100, 4:100, 5:100}
 	
-	t1 = datetime.now()
-	
-	print("Generating data for Basic %dx%d Rubik's Cube" % (m, n))
-
-	f = open(outpath, 'w')
-
-	for k in range(maxK):
-		shuffle = True
-		randomCube = Rubik(size=[m, n])
+	for m in range(2, 6):
+		n = m
+		e = 6 + (m - 2) * 2 + (n - 2)
 		
-		while(shuffle):
-			i = random.randint(0, maxK - 1)
-			newState = randomCube.doRandomActions(int(actionsCount[i]), low=0, high=2 * e - 1).stateInCsv()
-			
-			try:
-				key = data.values().index(newState)
-			except:
-				if randomCube.isNotSolved():
-					f.write(newState + '\n')
-					data.update({k:newState})
-					shuffle = False
+		allowedActionMin = 0
+		allowedActionMax = 2 * e - 1
+				
+		maxK = maxKValues.get(m)
+		data = dict() 
+		treeData = dict()
 	
-	t2 = datetime.now()
-	delta = t2 - t1
-	print("Finished generating data for Basic %dx%d Rubik's Cube:\n\t runtime = % d sec" % (m, n, delta.total_seconds()))
+		outpath = filedir + '/basic' + str(m) + 'x' + str(n) + 'RubikCube.csv'
+	
+		t1 = datetime.now()
+		print('\n=================================================================================================')
+		print("Generating data for Basic %dx%d Rubik's Cube" % (m, n))
 
-	f.close()
-					
+		f = open(outpath, 'w')
+		randomCube = Rubik(size=[m, n])
+		root = AnyNode(id='root', state=None)		
+		minSteps = np.Infinity
+		maxSteps = 0
+		solvedStateId = 0
+		for k in range(maxK):
+			shuffle = True
+			randomCube.resetState()
+			randomCube.spinRubik(e)
+			kSolveState = randomCube.stateInCsv()
+			try:
+				kRoot = treeData[kSolveState]['node']
+			except:
+				kRootId = 'SolvedState:' + str(solvedStateId)
+				solvedStateId = solvedStateId + 1
+				kRoot = AnyNode(id=kRootId, parent=root, state=kSolveState)
+				treeData.update({kSolveState:{'node':kRoot, 'parent':root}})
+				data.update({kRootId:kSolveState})
+				f.write('0,' + kSolveState + '\n')
+				
+			while(shuffle):
+				i = random.randint(minActions[m], maxActions[m])
+				for j in range(i):
+					oldState = randomCube.stateInCsv()
+					newState = randomCube.doRandomActions(low=allowedActionMin, high=allowedActionMax).stateInCsv()
+					try:
+						newNode = treeData[newState]['node']
+					except:
+						oldNode = treeData[oldState]['node']
+						newNodeId = 'Action:' + str(randomCube.latestAction)
+						newNode = AnyNode(id=newNodeId, parent=oldNode, state=newState)
+						treeData.update({newState:{'node':newNode, 'parent':oldNode}})
+
+				try:
+					key = list(data.values()).index(newState)
+				except:
+					if randomCube.isNotSolved():
+						steps = newNode.depth - kRoot.depth
+						f.write(str(steps) + ',' + newState + '\n')
+						data.update({k:newState})
+						if steps < minSteps:
+							minSteps = steps
+						if steps > maxSteps:
+							maxSteps = steps						
+						# print('\tBasic%dx%dRubikCube: k = %6d i = %4d moves = %4d minSteps = %4d maxSteps = %4d' % (m, n, k, i, steps, minSteps, maxSteps))
+						shuffle = False
+					else:
+						minSteps = 0
+	
+		t2 = datetime.now()
+		delta = t2 - t1
+		print("First Pass:Finished generating data for Basic %dx%d Rubik's Cube:\n\tRun Time = % d sec" % (m, n, delta.total_seconds()))
+		# print(RenderTree(root))
+		print("Consolidating generated Tree Data for Basic %dx%d Rubik's Cube" % (m, n))
+		t1 = t2
+
+		f.close()
+		i = 1
+		iParentNodes = [root] 
+		print('\tStarting: Tree Height = %d' % root.height)
+		while i <= root.height:
+			iNodes = []
+			for pNode in iParentNodes:
+				iNodes = iNodes + [c for c in pNode.children]
+			# iNodes = findall_by_attr(root, value=i, name='depth')	
+			for iNode in iNodes:	
+				oldState = iNode.state
+				for a in range(allowedActionMax):
+					randomCube.setState(oldState.split(','))
+					edge = a % ((allowedActionMax + 1) / e)
+					action = int(a / e)
+					randomCube.doAction(edge, action)
+					newState = randomCube.stateInCsv()
+					if newState in list(treeData.keys()):
+						iNode_a = treeData[newState]['node']
+						oldParent = treeData[newState]['parent']
+						newParent = iNode
+						if oldParent != iNode and oldParent.depth > i:
+							iNode_a.id = 'Action:' + str(a)
+							# #detach iNode_a from children of oldParent
+							oldParent.children = [c for c in oldParent.children if c.state != iNode_a.state]
+							# #attache of iNode_a to children of newParent 
+							iNode_a.parent = None
+							iNode_a.parent = newParent
+							treeData[newState]['parent'] = newParent
+			iParentNodes = iNodes
+			i = i + 1
+			
+		print('\tEnding: Tree Height = %d' % root.height)
+		t2 = datetime.now()
+		delta = t2 - t1
+		print("Finished Consolidating generated Tree Data for Basic %dx%d Rubik's Cube:\n\tRun Time = % d sec" % (m, n, delta.total_seconds()))		
+		t1 = t1
+		outpath = filedir + '/basic' + str(m) + 'x' + str(n) + 'RubikCubeOptimized.csv'
+		f = open(outpath, 'w')
+		minSteps = np.Infinity
+		maxSteps = 0
+		i = 1
+		for k in data.keys():
+			kState = data[k]
+			kNode = treeData[kState]['node']
+			steps = kNode.depth - 1
+			f.write(str(steps) + ',' + kState + '\n')
+			if steps < minSteps:
+				minSteps = steps
+			if steps > maxSteps:
+				maxSteps = steps			
+			# print('\tBasic%dx%dRubikCubeOptimized: index = %6d moves = %4d minSteps = %4d maxSteps = %4d' % (m, n, i, steps, minSteps, maxSteps))
+			i = i + 1
+			
+		t2 = datetime.now()
+		delta = t2 - t1
+		print("Second Pass:Finished generating data for Basic %dx%d Rubik's Cube:\n\tRun Time = % d sec" % (m, n, delta.total_seconds()))
